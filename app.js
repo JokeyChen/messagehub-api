c08baaa6264682fe860438de611b9e94e04c4c37
JokeyChen <jokey.chen@gmail.com>
2017-07-27 13:49:38 +0800

    Better error handling

--- app.js
+++ app.js
@@ -3,19 +3,53 @@ var mongoose = require('mongoose')
 var morgan = require('morgan')
 var cors = require('cors')
 var bodyParser = require('body-parser')
+var errorhandler = require('errorhandler')
 var app = express()
+
+var isProduction = process.env.NODE_ENV === 'production'
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(morgan('tiny'))
 
-mongoose.connect('mongodb://production:production@ds119223.mlab.com:19223/messagehub')
+if (!isProduction) {
+  app.use(errorhandler())
+}
+
+if (!isProduction) {
+  mongoose.connect('mongodb://production:production@ds119223.mlab.com:19223/messagehub')
+}
 
 require('./models/Message')
 app.use('/', require('./routes'))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  console.log('here')
+  var err = new Error('Route Not Found')
+  err.status = 404
+  next(err)
+})
+
+/// error handlers
+
+// development error handler
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack)
+    res.status(err.status || 500)
+    res.json({ error: err.message })
+  })
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({ error: err.message })
+})
+
+// finally, let's start our server...
+var server = app.listen( process.env.PORT || 3000, function(){
+  console.log('Listening on port ' + server.address().port)
 })
--- routes/messages.js
+++ routes/messages.js
@@ -4,39 +4,34 @@ var mongoose = require('mongoose')
 var Message = mongoose.model('Message')
 
 
-router.post('/', function (req, res) {
-  var newMessage = new Message()
+router.post('/', function (req, res, next) {
+  var newMessage = new Message({ body: req.body.body })
   newMessage.generateKey()
-  newMessage.body = req.body.body
-  console.log(newMessage.createdAt)
 
-  newMessage.save(function (err, message) {
-    if (err) return console.error(err)
+  newMessage.save().then(function (message) {
     res.status(201).json({
       key: message.key,
       body: message.body,
       created_at: message.createdAt
     })
-  })
+  }).catch(next)
 })
 
-router.get('/:key', function (req, res) {
-  Message.findOne({ key: req.params.key }, function (err, message) {
-    if (err) res.status(404).json({ error: 'Message Not Found' })
+router.get('/:key', function (req, res, next) {
+  Message.findOne({ key: req.params.key }).then(function (message) {
+    if (!message) res.status(404).json({ error: 'Message Not Found' })
     res.status(200).json({
       key: message.key,
       body: message.body,
       created_at: message.createdAt
     })
-  })
+  }).catch(next)
 })
 
-router.delete('/:key', function (req, res) {
-  console.log(req.params.key)
-  Message.deleteOne({ key: req.params.key }, function (err) {
-    if (err) res.status(404).json({ error: 'Message Not Found' })
+router.delete('/:key', function (req, res, next) {
+  Message.deleteOne({ key: req.params.key }).then(function () {
     res.sendStatus(204)
-  })
+  }).catch(next)
 })
 
 module.exports = router
