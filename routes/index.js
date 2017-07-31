44d3c21a6180b84f18544d1ddc1682195909f216
JokeyChen <jokey.chen@gmail.com>
2017-07-31 11:40:30 +0800

    Add welcome at root call

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
+router.get('/', function (req, res, next) {
+  res.json({success: 'Welcome to MessageHub API'})
+})
+
 router.use('/messages', require('./messages'))
 
 module.exports = router
